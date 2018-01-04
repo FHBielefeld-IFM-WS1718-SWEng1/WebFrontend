3819555845df3e6ae375519c57ba2864b4347dcc
Patrick Poppe <patrick.poppe@fh-bielefeld.de>
2018-01-04 16:07:28 +0100

    Einbau des eingeloggten Usernamens in die Headerzeile bei Veranstaltung

---
+++ js/header.js
@@ -0,0 +1,16 @@
+const naviVue = new Vue({
+    el: '#navi',
+    data: {
+        name: "Torwart"
+    }
+});
+
+var loggedInId = localStorage.getItem("userId");
+var split = /(id=)(\d+)/g.exec(window.location.href);
+var userId = (split != null && split.length > 0) ? split[2] : loggedInId;
+var apiKey = localStorage.getItem("apiKey");
+getRequest("user/" + userId + "?api=" + apiKey, function (data) {
+    if (!data.error) {
+        naviVue.name = data.name;
+    }
+});
