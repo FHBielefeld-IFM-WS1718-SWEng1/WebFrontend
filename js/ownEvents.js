665c6b267e14880bf78a46474384413063cbe4e0
Daniel R <daniel_felix.raeder@fh-bielefeld.de>
2018-01-09 22:47:05 +0100

    ownEvent in Plural geändert Party listing via v-for

---
+++ js/ownEvents.js
@@ -0,0 +1,28 @@
+const contentVue = new Vue({
+    el: '#content',
+    data: {
+        timeOptions: {year: 'numeric', month: 'short', day: 'numeric'},
+        parties: []
+    },
+    methods: {
+        getPartyTime(party) {
+            let time = new Date(party.startDate);
+            return time.toLocaleDateString('de-DE', this.timeOptions);
+        }
+    },
+    created: function () {
+        let apiKey = localStorage.getItem("apiKey");
+        console.log("api: "+apiKey);
+        let dateNow = new Date();
+        getRequest("party?api=" + apiKey + "&creator=true", function (data) {
+            if (!data.error && data.parties) {
+                for (let i in data.parties) {
+                    let party = data.parties[i];
+                    let date = new Date(party.endDate ? party.endDate : party.startDate);
+                    if (date > dateNow)//Nur Parties anzeigen die noch nicht vorbei sind
+                        contentVue.parties.push(party);
+                }
+            }
+        });
+    }
+});
