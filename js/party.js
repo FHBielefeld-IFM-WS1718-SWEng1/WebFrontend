932c647d0170ace73c77fe3dfb3787d593351ccd
ppoppe1 <33016528+ppoppe1@users.noreply.github.com>
2018-01-17 23:36:08 +0100

    Owner/Guest View eingefügt (#29)
    Party Website unterscheidet jetzt zwischen einer Owner-Ansicht und einer Guest-Ansicht

--- js/party.js
+++ js/party.js
@@ -1,11 +1,14 @@
 const contentVue = new Vue({
     el: '#party_description',
     data: {
-        partyname: "Testparty",
-        gastgeber: "PartyPlaner",
-        ort: "D329",
-        uhrzeit: "28.09.2018 17 Uhr",
-        beschreibung: "Testbeschreibung"
+        partyname: "",
+        gastgeber: "",
+        ort: "",
+        startzeit: "",
+        endzeit: "",
+        beschreibung: "",
+        owner: false
+
     },
     methods: {
         mapsAufrufen() {
@@ -14,5 +17,37 @@ const contentVue = new Vue({
         }
     }
 });
+var options = {
+    year: "numeric", month: "short",
+    day: "numeric", hour: "2-digit", minute: "2-digit"
+};
 
-//https://www.google.com/maps?daddr=Berlin
+var owner = false;
+
+var apiKey = localStorage.getItem("apiKey");
+var split = /(id=)(\d+)/g.exec(window.location.href);
+var partyId = (split != null && split.length > 0) ? split[2] : loggedInId;
+getRequest("party/" + partyId + "?api=" + apiKey, function (data) {
+    if (!data.error) {
+        contentVue.partyname = data.name;
+        contentVue.beschreibung = data.description;
+        contentVue.gastgeber = data.ersteller.name;
+        contentVue.ort = data.location;
+        if (data.startDate != null) {
+            var date = new Date(data.startDate);
+            contentVue.startzeit = date.toLocaleDateString("de-de", options);
+        }
+        if (data.endDate != null) {
+            date = new Date(data.endDate);
+            contentVue.endzeit = date.toLocaleDateString("de-de", options)
+        }
+        if (data.ersteller.id == localStorage.getItem("userId")) {
+            contentVue.owner = true;
+        }
+        else {
+            contentVue.owner = false;
+        }
+        contentVue.ort = this.owner;
+    }
+
+});
